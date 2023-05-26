0b1d24f8e2d9824ae917c7ad69b051db7ec30bd6
Bruno Silva <onurb.hsilva@gmail.com>
2023-05-26 20:48:45 -0300

    create getidProduto in ProdutoTest.java

--- SistemaFarmacia/src/source/Produto.java
+++ SistemaFarmacia/src/source/Produto.java
@@ -2,14 +2,20 @@ package source;
 
 public class Produto {
 
+    private int idProduto;
     private String nome;
 
-    public  Produto(String nome) {
+    public  Produto(int idProduto, String nome) {
+        this.idProduto = idProduto;
         this.nome = nome;
     }
 
+    public int getidProduto() {
+        return this.idProduto;
+    }
+
     public String getNome() {
-        return nome;
+        return this.nome;
     }
 
 }
--- SistemaFarmacia/src/test/ProdutoTest.java
+++ SistemaFarmacia/src/test/ProdutoTest.java
@@ -5,8 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 class ProdutoTest {
     @Test
+    void getidProduto() {
+        Produto produto1 = new Produto(1, "Doril");
+        assertEquals(1, produto1.getidProduto());
+    }
+    @Test
     void  getName() {
-        Produto produto1 = new Produto("Doril");
+        Produto produto1 = new Produto(1, "Doril");
         assertEquals("Doril", produto1.getNome());
     }
 }
