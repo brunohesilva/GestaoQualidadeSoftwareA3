da254459dd2abb07bb227d39b2d2bee7c13af836
Bruno Silva <onurb.hsilva@gmail.com>
2023-05-26 21:38:08 -0300

    create getMarca in ProdutoTest.java

--- SistemaFarmacia/src/source/Produto.java
+++ SistemaFarmacia/src/source/Produto.java
@@ -5,11 +5,13 @@ public class Produto {
     private int idProduto;
     private String nome;
     private String descricao;
+    private String marca;
 
-    public  Produto(int idProduto, String nome,String descricao) {
+    public  Produto(int idProduto, String nome,String descricao, String marca) {
         this.idProduto = idProduto;
         this.nome = nome;
         this.descricao = descricao;
+        this.marca = marca;
     }
 
     public int getidProduto() { return this.idProduto; }
@@ -19,4 +21,6 @@ public class Produto {
     }
 
     public String getDescricao() { return this.descricao; }
+    public String getMarca() { return this.marca; }
+
 }
--- SistemaFarmacia/src/test/ProdutoTest.java
+++ SistemaFarmacia/src/test/ProdutoTest.java
@@ -6,17 +6,22 @@ import org.junit.jupiter.api.Test;
 class ProdutoTest {
     @Test
     void getidProduto() {
-        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu");
+        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu", "Bayer");
         assertEquals(1, produto1.getidProduto());
     }
     @Test
     void  getName() {
-        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu");
+        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu", "Bayer");
         assertEquals("Doril", produto1.getNome());
     }
     @Test
     void getDescricao() {
-        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu");
+        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu", "Bayer");
         assertEquals("Tomou doril a dor sumiu", produto1.getDescricao());
+    }
+    @Test
+    void getMarca() {
+        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu", "Bayer");
+        assertEquals("Bayer", produto1.getMarca());
     }
 }
