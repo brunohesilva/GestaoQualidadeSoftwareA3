eb12d408975c97728811b78694932616eb1695c7
Bruno Silva <onurb.hsilva@gmail.com>
2023-05-26 21:30:52 -0300

    create getDescricao in ProdutoTest.java

--- SistemaFarmacia/src/source/Produto.java
+++ SistemaFarmacia/src/source/Produto.java
@@ -4,18 +4,19 @@ public class Produto {
 
     private int idProduto;
     private String nome;
+    private String descricao;
 
-    public  Produto(int idProduto, String nome) {
+    public  Produto(int idProduto, String nome,String descricao) {
         this.idProduto = idProduto;
         this.nome = nome;
+        this.descricao = descricao;
     }
 
-    public int getidProduto() {
-        return this.idProduto;
-    }
+    public int getidProduto() { return this.idProduto; }
 
     public String getNome() {
         return this.nome;
     }
 
+    public String getDescricao() { return this.descricao; }
 }
--- SistemaFarmacia/src/test/ProdutoTest.java
+++ SistemaFarmacia/src/test/ProdutoTest.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.Test;
 class ProdutoTest {
     @Test
     void getidProduto() {
-        Produto produto1 = new Produto(1, "Doril");
+        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu");
         assertEquals(1, produto1.getidProduto());
     }
     @Test
     void  getName() {
-        Produto produto1 = new Produto(1, "Doril");
+        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu");
         assertEquals("Doril", produto1.getNome());
+    }
+    @Test
+    void getDescricao() {
+        Produto produto1 = new Produto(1, "Doril", "Tomou doril a dor sumiu");
+        assertEquals("Tomou doril a dor sumiu", produto1.getDescricao());
     }
 }
