2d59ea3c81bfa79f2b059ee15422dd3fb890a11f
Bruno Silva <onurb.hsilva@gmail.com>
2023-05-27 16:33:55 -0300

    getNomeRemedio test

--- SistemaFarmacia/src/source/Receita.java
+++ SistemaFarmacia/src/source/Receita.java
@@ -8,12 +8,14 @@ public class Receita {
     private String nomePaciente;
     private String nomeMedico;
     private String dataValidade;
+    private String nomeRemedio;
 
-    public Receita(int idReceita, String nomePaciente, String nomeMedico, String dataValidade) {
+    public Receita(int idReceita, String nomePaciente, String nomeMedico, String dataValidade, String nomeRemedio) {
         this.idReceita = idReceita;
         this.nomePaciente = nomePaciente;
         this.nomeMedico = nomeMedico;
         this.dataValidade = dataValidade;
+        this.nomeRemedio = nomeRemedio;
     }
 
     public int getIdReceita() {
@@ -27,6 +29,9 @@ public class Receita {
     }
     public String getDataValidade() {
         return this.dataValidade;
+    }
+    public String getNomeRemedio() {
+        return this.nomeRemedio;
     }
 
 }
--- SistemaFarmacia/src/test/ReceitaTest.java
+++ SistemaFarmacia/src/test/ReceitaTest.java
@@ -9,24 +9,29 @@ import java.text.SimpleDateFormat;
 class ReceitaTest {
 
     @Test
-    void getIdReceita(){
-        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro");
+    void getIdReceita() {
+        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro", "Doril");
         assertEquals(1, receita1.getIdReceita());
     }
     @Test
     void getNomePaciente() {
-        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro");
+        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro", "Doril");
         assertEquals("Bruno", receita1.getNomePaciente());
     }
     @Test
     void getNomeMedico() {
-        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro");
+        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro", "Doril");
         assertEquals("Dr. Hans Chucrute", receita1.getNomeMedico());
     }
     @Test
-    void getData(){
-        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro");
+    void getData() {
+        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro", "Doril");
         assertEquals("17 de Novembro", receita1.getDataValidade());
+    }
+    @Test
+    void getNomeRemedio() {
+        Receita receita1 = new Receita(1, "Bruno", "Dr. Hans Chucrute", "17 de Novembro", "Doril");
+        assertEquals("Doril", receita1.getNomeRemedio());
     }
 
 }
