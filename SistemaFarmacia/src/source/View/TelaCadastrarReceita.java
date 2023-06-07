b27b0d9b0d3762c1ab4b909e83e11710e108c999
João Lucas Yonea <assasinomortal321321@gmail.com>
2023-06-06 22:26:12 -0300

    implementações na interface

---
+++ SistemaFarmacia/src/source/View/TelaCadastrarReceita.java
@@ -0,0 +1,15 @@
+package source.View;
+
+import javax.swing.*;
+
+public class TelaCadastrarReceita {
+    private JTextField NomeRemedioTextField;
+    private JTextField UsoTextField;
+    private JButton enviarButton;
+    private JButton voltarButton;
+    private JTextField CRMTextField;
+    private JTextField NomePacienteTextField;
+    private JPanel CadastroReceitaPanel;
+    private JTextField NomeMedicoTextField;
+    private JTextField DataTextField;
+}
---
+++ SistemaFarmacia/src/source/View/TelaEditarProduto.java
@@ -0,0 +1,16 @@
+package source.View;
+
+import javax.swing.*;
+
+public class TelaEditarProduto {
+    private JPanel EditarProdutoPanel;
+    private JTextField NomeTextField;
+    private JTextField DescricaoTextField;
+    private JTextField MarcaTextField;
+    private JTextField TipoTextField;
+    private JTextField QuantidadeTextField;
+    private JTextField PesoTextField;
+    private JButton voltarButton;
+    private JRadioButton necessárioReceitaRadioButton;
+    private JButton enviarButton;
+}
